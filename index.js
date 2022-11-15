f54f9d93dc0be217897b077ba26557c6d00a9759
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-11-15 22:16:37 +0400

    feat: add infinite scrolling

---
+++ index.js
@@ -0,0 +1,98 @@
+// task: to implement infinite scrolling
+
+const URL = 'https://picsum.photos/v2/list';
+
+const fetchImg = (() => {
+  let page = 1;
+  return async function (url, limit = 10) {
+    const req = await fetch(`${url}?page=${page}&limit=${limit}`);
+    page = page + 1;
+    return await req.json();
+  }
+})()
+
+// lazy loading observer
+const lazyLoadingObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const card = entry.target;
+
+      // change card background image and image placeholder to real image
+      card.style.background = 'deepskyblue'
+      const img = card.querySelector('img');
+      
+      img.src = img.dataset.src;
+      img.removeAttribute('data-src');
+
+      observer.unobserve(card);
+    }
+  })
+}, {
+  rootMargin: '0px',
+  threshold: .5
+});
+
+// infinite scroll observer
+const infiniteScrollObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // unobserve previous last element
+      observer.unobserve(entry.target);
+
+      const imgs = fetchImg(URL)
+        .then(res => {
+          for (let index = 0; index < res.length; index++) {
+
+            const card = createCard(res[index].download_url, res[index].author);
+            // set lazyLoading observer to each new card
+            lazyLoadingObserver.observe(card);
+
+            // add infiniteScrollObserver to the new last card
+            if (index === res.length - 1) {
+              infiniteScrollObserver.observe(card);
+            }
+          
+            document.querySelector('#root').appendChild(card);
+          }
+        })
+      .catch(err => console.log('error:', err))
+    }
+    
+  })
+}, {
+  threshold: 1,
+})
+
+function createCard(imgUrl, text) {
+  const cardTemplate = document.querySelector('#card-template');
+  const clone = cardTemplate.content.cloneNode(true)
+  const card = clone.querySelector('.card');
+
+  const image = card.querySelector('img');
+  image.dataset.src = imgUrl;
+  const caption = card.querySelector('.card__text');
+  image.src = './placeholder-img.png';
+  caption.textContent = text;
+
+  return card;
+}
+
+window.onload = async () => {
+  const imgs = await fetchImg(URL);
+  const rootEl = document.querySelector('#root');
+
+  for (let index = 0; index < imgs.length; index++) {
+
+    const card = createCard(imgs[index].download_url, imgs[index].author);
+    // add lazy loading observer to each card
+    lazyLoadingObserver.observe(card);
+
+    // add infiniteScrollObserver to the last card
+    if (index === imgs.length - 1) {
+      infiniteScrollObserver.observe(card);
+    }
+
+    rootEl.appendChild(card);
+  }
+}
+
